47c3558fb42276e57f5c881a9fb51522e7521afc
Jordan Sleeper <survivingknowledge@gmail.com>
2017-09-07 08:45:56 -0400

    main products outline

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render(){
+    return(
+      <div className="main ui text container">
+        <h1 className="ui dividing centered header">Popular Products</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
