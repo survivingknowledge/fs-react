9d098d6dbb6550f871a368f41d6ad7fc08562246
Jordan Sleeper <survivingknowledge@gmail.com>
2017-09-07 08:53:04 -0400

    first component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import ProductList from './app-1';
+
 class App extends Component {
   render(){
     return(
       <div className="main ui text container">
         <h1 className="ui dividing centered header">Popular Products</h1>
+        <ProductList />
       </div>
     );
   }
