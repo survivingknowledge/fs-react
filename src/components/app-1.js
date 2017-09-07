d3e30d0f6e0b5cefd5c7dac5d6901f7f346e8af5
Jordan Sleeper <survivingknowledge@gmail.com>
2017-09-07 08:59:56 -0400

    Product component

--- src/components/app-1.js
+++ src/components/app-1.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import Product from './app-2';
+
 class ProductList extends Component {
   render() {
     return (
       <div className="ui unstackable items">
-        Hello, friend! I am a basic React Component.
+        <Product />
       </div>
     );
   }
---
+++ src/components/app-2.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Product extends Component {
+  render() {
+    return (
+      <div className="item">
+        <div className="image">
+          <img src="images/products/image-aqua.png" alt=""/>
+        </div>
+        <div className="middle aligned content">
+          <div className="description">
+            <a href="">Fort Knight</a>
+            <p>Authentic renaissance actors, delivered in just two weeks.</p>
+          </div>
+          <div className="extra">
+            <span>Submitted by:</span>
+            <img src="images/avatars/daniel.jpg" alt="" className="ui avatar image"/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+ export default Product;
